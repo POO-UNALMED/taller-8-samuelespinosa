5c10ca5b6fddc32cca21ebbd9c8ceab4af5d4bcf
samuelespinosa <saiorcore@gmail.com>
2020-10-18 00:04:15 -0500

    uno

---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,33 @@
+package futbol;
+public class Jugador extends Futbolista {
+	public short golesMarcados;
+	public byte dorsal;
+	public Jugador() {
+		golesMarcados=289;
+		dorsal=7;
+	}
+
+	public Jugador(String nombre, int edad, String posicion,short golesMarcados,byte dorsal) {
+		super(nombre, edad, posicion);
+		this.dorsal=dorsal;
+		this.golesMarcados=golesMarcados;
+	}
+
+	@Override
+	public boolean jugarConLasManos() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int compareTo(Futbolista f) {
+		// TODO Auto-generated method stub
+		return this.getEdad()-f.getEdad();
+	}
+	
+	public String toString() {
+		return "El futbolista "+this.getNombre()+" tiene "+this.getEdad()+", y juega de "+this.getPosicion()+" con el dorsal "+dorsal+". Ha marcado "+golesMarcados+" goles";
+	}
+	
+
+}
---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,34 @@
+package futbol;
+public class Portero extends Futbolista {
+	public short golesRecibidos;
+	public byte dorsal;
+	public Portero() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Portero(String nombre, int edad, String posicion,short golesRecibidos,byte dorsal) {
+		super(nombre, edad, posicion);
+		this.dorsal=dorsal;
+		this.golesRecibidos=golesRecibidos;
+	}
+
+	@Override
+	public boolean jugarConLasManos() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public int compareTo(Futbolista f) {
+		
+			Portero p=(Portero) f;
+			return (int) this.golesRecibidos-p.golesRecibidos;
+		
+		
+	}
+	public String toString() {
+		return "El futbolista "+this.getNombre()+" tiene "+this.getEdad()+", y juega de "+this.getPosicion()+" con el dorsal "+dorsal+" .Le han marcado "+golesRecibidos;
+	}
+
+
+}
